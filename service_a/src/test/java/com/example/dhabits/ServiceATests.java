fd04e562a206d9090448ed5de2908af08f87736c
thefasttracker <thefasttracker@gmail.com>
2021-06-28 00:50:08 +0300

    edit Readme

--- service_a/src/test/java/com/example/dhabits/ServiceATests.java
+++ service_a/src/test/java/com/example/dhabits/ServiceATests.java
@@ -3,11 +3,18 @@ package com.example.dhabits;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class ServiceBTests {
+class ServiceATests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testOne() {
+        assertEquals(4, "Duke".length());
     }
 
 }
